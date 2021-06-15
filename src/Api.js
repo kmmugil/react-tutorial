3badb23743821140d11e463e6afb3844a38e8b46
kmmugil <kmm.mugil@gmail.com>
2021-06-16 00:57:57 +0530

    Fetching data from API

---
+++ src/Api.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class App extends React.Component{
+    state = {
+        data: [],
+    }
+
+    componentDidMount(){
+        const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*&limit=1';
+        fetch(url).then((result) => result.json()).then((result) => {
+            this.setState({
+                data: result,
+            })
+        });
+    }
+
+    render(){
+        const {data} = this.state;
+        const result = data.map((entry, index) => {
+            return <li key={index}>{entry}</li>
+        });
+        return <div className="container"><ul>{result}</ul></div>;
+    }
+}
+
+export default App;
+
